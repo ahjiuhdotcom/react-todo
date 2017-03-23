aed8682b0e57c8658e7679435be801eb0786e686
ahjiuhdotcom <ahjiuhdotcom@yahoo.com>
2017-03-24 05:49:10 +1100

    Add statis todo list

---
+++ app/components/TodoList.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+  render: function() {
+
+    var {todos} = this.props;
+
+    var renderTodos = () => {
+      return todos.map((todo) => {
+        // {...todo} is using spread operator
+        // it spread out all the property in the object
+        // In this case it spread out the todo object into individual props
+        // so that all attributes in todo is passed down tp child
+        return (
+          <Todo key={todo.id} {...todo}/>
+        );
+      });
+    };
+
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    );
+  }
+});
+
+module.exports = TodoList;
